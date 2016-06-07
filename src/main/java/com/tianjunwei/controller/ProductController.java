3996a5a96775c4f43a2a35e14644a9d706b993ae
tiantianjunwei <924862077@qq.com>
2016-06-07 09:01:07 +0800

    完善示例

--- src/main/java/com/tianjunwei/controller/ProductController.java
+++ src/main/java/com/tianjunwei/controller/ProductController.java
@@ -4,28 +4,29 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.alibaba.fastjson.JSON;
 import com.tianjunwei.domain.Product;
 import com.tianjunwei.form.ProductForm;
 
 @Controller
+@RequestMapping("/product")
 public class ProductController {
 
     private static final Log logger = LogFactory.getLog(ProductController.class);
-    final String URL = "/product_input";
 
-    @RequestMapping(value = URL)
+    @RequestMapping(value = "input")
     public String inputProduct() {
         logger.info("inputProduct called");
-        return "ProductForm";
+        return "ProductForm";  //对应jsp页面名称
     }
     
-    @RequestMapping(value="/product_save")
+    @RequestMapping(value="/save")
     public String saveProduct(ProductForm productForm, Model model) {
         logger.info("saveProduct called");
-        // no need to create and instantiate a ProductForm
-        // create Product
         Product product = new Product();
         product.setName(productForm.getName());
         product.setDescription(productForm.getDescription());
@@ -34,10 +35,29 @@ public class ProductController {
                     productForm.getPrice()));
         } catch (NumberFormatException e) {
         }
-
-        // add product
-
         model.addAttribute("product", product);
-        return "ProductDetails";
+        return "ProductDetails";//对应jsp页面名称
     }
+    
+    @ResponseBody
+	@RequestMapping("/get")
+	public String get(){
+		Product product = new Product();
+		product.setDescription("hello swdf2 springMVC  RestFul");
+		product.setId(10);
+		product.setName("swdf2");
+		product.setPrice(10);
+		return JSON.toJSONString(product); //在页面中返回json数据
+	}
+    @ResponseBody
+	@RequestMapping("/delete/{id}")
+	public String delete(@PathVariable("id") long id){
+		Product product = new Product();
+		product.setDescription("hello swdf2 springMVC  RestFul");
+		product.setId(id);
+		product.setName("swdf2");
+		product.setPrice(10);
+		return JSON.toJSONString(product);//在页面中返回json数据
+	}
+    
 }

924f35de0009dad4a67fae834bf5db79bb70d3a4
tiantianjunwei <924862077@qq.com>
2016-08-11 18:19:49 +0800

    多语言locale放置在session中

--- src/main/java/com/tianjunwei/validator/ValidateController.java
+++ src/main/java/com/tianjunwei/validator/ValidateController.java
@@ -4,6 +4,7 @@ import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
 import javax.validation.Valid;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.support.RequestContext;
 
 import com.tianjunwei.validator.entity.ValidateModel;
 import com.tianjunwei.validator.service.ValidatorTestService;
@@ -36,11 +38,13 @@ public class ValidateController {
     }
     
     @RequestMapping(value="/form", method = {RequestMethod.POST})
-    public String test(Model model, @Valid @ModelAttribute("contentModel") ValidateModel validateModel, BindingResult result) throws NoSuchAlgorithmException{
+    public String test(HttpServletRequest request,Model model, @Valid @ModelAttribute("contentModel") ValidateModel validateModel, BindingResult result) throws NoSuchAlgorithmException{
         
         //如果有验证错误 返回到form页面
         if(result.hasErrors())
             return test(model);
+        RequestContext requestContext = new RequestContext(request);
+		model.addAttribute("success", requestContext.getMessage("success"));
         return "validator/validatesuccess";     
     }
     

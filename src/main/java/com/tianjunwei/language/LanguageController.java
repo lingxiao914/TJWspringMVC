dca00868be692c82cea1d7da725fc17f79025aa3
tiantianjunwei <924862077@qq.com>
2016-08-15 21:10:59 +0800

    将多语言设置放到handlerinterceptor中

--- src/main/java/com/tianjunwei/language/LanguageController.java
+++ src/main/java/com/tianjunwei/language/LanguageController.java
@@ -1,16 +1,12 @@
 package com.tianjunwei.language;
 
-import java.util.Locale;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.springframework.web.servlet.support.RequestContext;
 
 import tianjunwei.cache.redis.RedisTemplate;
@@ -25,25 +21,7 @@ public class LanguageController {
 	public String language(HttpServletRequest request, Model model) {
 		HttpSession session = request.getSession();
 		session.setAttribute("user", "tian");
-		String language = request.getParameter("language");
-		if (language != null&&language.equals("zh")) {
-			Locale locale = new Locale("zh", "CN");
-			request.getSession()
-					.setAttribute(
-							SessionLocaleResolver.LOCALE_SESSION_ATTRIBUTE_NAME,
-							locale);
-		} else if (language != null&&language.equals("en")) {
-			Locale locale = new Locale("en", "US");
-			request.getSession()
-					.setAttribute(
-							SessionLocaleResolver.LOCALE_SESSION_ATTRIBUTE_NAME,
-							locale);
-		} else {
-			request.getSession().setAttribute(
-					SessionLocaleResolver.LOCALE_SESSION_ATTRIBUTE_NAME,
-					LocaleContextHolder.getLocale());
-			language = LocaleContextHolder.getLocale().getLanguage();
-		}
+		String language = (String) request.getAttribute("language");
 		RequestContext requestContext = new RequestContext(request);
 		model.addAttribute("message", requestContext.getMessage("message"));
 		model.addAttribute("language", language);

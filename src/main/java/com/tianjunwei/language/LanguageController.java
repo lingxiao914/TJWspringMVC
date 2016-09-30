dc5bd25e1d4efd34447314fd94e2e674afe95382
tiantianjunwei <924862077@qq.com>
2016-09-30 13:52:43 +0800

    增加语言拦截器配置

--- src/main/java/com/tianjunwei/language/LanguageController.java
+++ src/main/java/com/tianjunwei/language/LanguageController.java
@@ -3,6 +3,7 @@ package com.tianjunwei.language;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,9 @@ public class LanguageController {
 		String language = request.getParameter("language");
 		RequestContext requestContext = new RequestContext(request);
 		model.addAttribute("message", requestContext.getMessage("message"));
+		if(StringUtils.isEmpty(language)){
+			language = "zh";
+		}
 		model.addAttribute("language", language);
 		return "jsp/language";
 	}

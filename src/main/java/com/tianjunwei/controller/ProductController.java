6d8d54edc662371e42eeb18d65923fd9133c8d19
tiantianjunwei <924862077@qq.com>
2017-02-15 20:35:08 +0800

    修改配置

--- src/main/java/com/tianjunwei/controller/ProductController.java
+++ src/main/java/com/tianjunwei/controller/ProductController.java
@@ -3,20 +3,21 @@ package com.tianjunwei.controller;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.alibaba.fastjson.JSON;
 import com.sun.beans.editors.LongEditor;
-import com.sun.org.apache.xml.internal.security.Init;
 import com.tianjunwei.controller.entity.Product;
 import com.tianjunwei.controller.entity.ProductForm;
 
+@Service
 @Controller
 @RequestMapping("/product")
 public class ProductController {
@@ -75,5 +76,16 @@ public class ProductController {
 		product.setPrice(10);
 		return product;//在页面中返回json数据
 	}
+    
+    @RequestMapping("/getdata")
+    @ResponseBody
+    public Object getData(String name,Integer age){
+    	return name+""+age;
+    }
+    
+    @RequestMapping("/viewName")
+    public Object viewName(){
+    	return null;
+    }
     
 }
--- src/main/java/com/tianjunwei/redirect/RedirectController.java
+++ src/main/java/com/tianjunwei/redirect/RedirectController.java
@@ -1,5 +1,7 @@
 package com.tianjunwei.redirect;
 
+import java.util.Random;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/book")
@@ -14,25 +17,33 @@ import org.springframework.web.bind.support.SessionStatus;
 public class RedirectController {
 	
 	@RequestMapping("/index")
-	public String index(Model model){
+	public String index(Model model,RedirectAttributes attr){
+		attr.addAttribute("attributeName", "attributeValue");
 		model.addAttribute("book", "金刚经");
-		model.addAttribute("description","不擦擦擦擦擦擦擦车");
+		model.addAttribute("description","不擦擦擦擦擦擦擦车"+new Random().nextInt(100));
 		model.addAttribute("price", new Double("1000.00"));
+		model.addAttribute("attributeName1", "attributeValue1");
+		//跳转之前将数据保存到book、description和price中，因为注解@SessionAttribute中有这几个参数
 		return "redirect:get.action";
 	}
 	
 	@RequestMapping("/get")
-	public String get(@ModelAttribute ("book") String book,ModelMap model,
-			SessionStatus sessionStatus){
+	public String get(@ModelAttribute ("book") String book,ModelMap model,@ModelAttribute("attributeName") String attributeName,
+			@ModelAttribute("attributeName1") String attributeName1,SessionStatus sessionStatus){
+		//可以获得book、description和price的参数
 		System.out.println(model.get("book")+";"+model.get("description")+";"+model.get("price"));
 		sessionStatus.setComplete();
+		System.out.println(attributeName);
+		System.out.println(attributeName1);
 		return "redirect:complete.action";
 	}
 	
 	@RequestMapping("/complete")
 	public String complete(ModelMap modelMap){
+		//已经被清除，无法获取book的值
 		System.out.println(modelMap.get("book"));
-		return "index";
+		modelMap.addAttribute("book", "妹纸");
+		return "sessionAttribute";
 	}
 
 }
